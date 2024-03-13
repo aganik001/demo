ecb48bb93abf667ac109ba7fd07b63497dbf1546
agank001 <aganik.9brainzgmail.com>
2024-03-13 19:26:19 +0530

    make a program screen 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         archivo: "'Archivo Narrow', sans-serif !important",
       },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotatey(0deg)' },
+          '100%': { transform: 'rotatey(180deg)' },
+        },
+      },
+      animation: {
+        'waving-hand': 'wave  0.6s ease-in-out 0.2s  alternate ',
+      },
     },
   },
   plugins: [],
